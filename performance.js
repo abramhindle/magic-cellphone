a97ce3c1f31d06b9e0371abd892c1fb54358cb89
Abram Hindle <abram.hindle@softwareprocess.es>
2017-08-25 21:33:58 -0600

    delayer performance

--- performance.js
+++ performance.js
@@ -5,12 +5,12 @@ function checkAndInstallPerformance(nextTime,nextNextURL) {
     if (parsedUrl.searchParams.has("action")) {
         if (parsedUrl.searchParams.get("action") === "perform") {
             var time = 1000*parseFloat(parsedUrl.searchParams.get("time"));
-            var nextLocation = parsedUrl.searchParams.get("next");
+            // var nextLocation = parsedUrl.searchParams.get("next");
             var params = new URLSearchParams();
             params.set("action","perform");
             params.set("time",nextTime);
-            params.set("next",nextNextURL);
-            var nextURL = nextLocation + "?" + params.toString();
+            //params.set("next",nextNextURL);
+            var nextURL = nextNextURL + "?" + params.toString();
             console.log("setting timeout to transition to " + nextURL + " in " + time  + " milliseconds");
             performTimeout = setTimeout(function() {
                 console.log("Transition" + nextURL + " after " + time  + " milliseconds");
